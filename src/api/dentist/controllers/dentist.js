6222c3382ffb8190aeca65b1d766a4d5ad7559d5
imrandev20 <imrandev20@gmail.com>
2022-06-15 13:32:58 +0600

    added header

---
+++ src/api/dentist/controllers/dentist.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  dentist controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::dentist.dentist');
